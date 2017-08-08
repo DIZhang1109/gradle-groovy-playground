19a6d3873b47052b41bd913b690c976e2e6f04dc
Di Zhang <di.zhang1109@gmail.com>
2017-08-09 10:50:27 +1200

    Move cucumber options set up to build.gralde

---
+++ src/main/groovy/example/Speak.groovy
@@ -0,0 +1,15 @@
+package example
+
+/**
+ * Created by Di on 9/08/17.
+ * Speak
+ */
+class Speak {
+    static void speak(word) {
+        println "Speak $word"
+    }
+
+    static void speakQuiet(word) {
+        println "Hu... $word"
+    }
+}
--- src/test/resources/example/shout.feature
+++ src/test/resources/example/shout.feature
@@ -1,5 +1,7 @@
+@all
 Feature: Shout
 
+  @shout
   Scenario Outline: Shout on the screen
     Given I want to shout <Word>
     Then <Word> will be louder
