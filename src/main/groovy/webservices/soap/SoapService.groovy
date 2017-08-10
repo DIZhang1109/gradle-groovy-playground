30f9c95d5c69e4d7831b9e8e9f6a1bafcd62def7
zhangd <di.zhang1109@gmail.com>
2017-08-10 15:13:28 +1200

    Add simple log4j support

--- src/main/groovy/webservices/soap/SoapService.groovy
+++ src/main/groovy/webservices/soap/SoapService.groovy
@@ -1,5 +1,6 @@
 package webservices.soap
 
+import groovy.util.logging.Slf4j
 import wslite.soap.SOAPClient
 import wslite.soap.SOAPResponse
 import wslite.soap.SOAPVersion
@@ -8,14 +9,18 @@ import wslite.soap.SOAPVersion
  * Created by Di on 9/08/17.
  * SoapService
  */
+
+@Slf4j
 class SoapService {
     SOAPClient client
 
     void initiateSOAPClient() {
+        log.info 'Instantiate a new SOAPClient '
         client = new SOAPClient('http://www.holidaywebservice.com/Holidays/US/Dates/USHolidayDates.asmx')
     }
 
     SOAPResponse getSOAPV1Response(name, searchYear) {
+        log.info "Send SOAP 1.1 request through $name and $searchYear, then return the response"
         client.send(SOAPAction: "http://www.27seconds.com/Holidays/US/Dates/$name") {
             body {
                 "$name"('xmlns': 'http://www.27seconds.com/Holidays/US/Dates/') {
@@ -26,6 +31,7 @@ class SoapService {
     }
 
     SOAPResponse getSOAPV2Response(name, searchYear) {
+        log.info "Send SOAP 1.2 request through $name and $searchYear, then return the response"
         client.send(SOAPVersion.V1_2,
                 """<?xml version="1.0" encoding="utf-8"?>
                     <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
