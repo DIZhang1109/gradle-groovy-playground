b4ce73b0b77b2230cdb9691936ebac2749dba03a
zhangd <di.zhang1109@gmail.com>
2017-08-14 10:10:49 +1200

    Small fix in log info

--- src/main/groovy/webservices/soap/SoapService.groovy
+++ src/main/groovy/webservices/soap/SoapService.groovy
@@ -72,7 +72,7 @@ class SoapService {
     }
 
     SOAPResponse getCalculatorSOAPV2Response(action, digit1, digit2) {
-        log.info "Send SOAP 1.1 request through $digit1 and $digit2, then return the response"
+        log.info "Send SOAP 1.2 request through $digit1 and $digit2, then return the response"
         client.send(SOAPVersion.V1_2,
                 """<?xml version="1.0" encoding="utf-8"?>
                     <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
